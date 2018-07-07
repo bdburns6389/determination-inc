c5dcae42572baeff8e715d283106df2d1a54ab27
Brian Burns <bdburns6389@gmail.com>
2018-07-07 00:17:12 -0500

    Adding styling in Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -16,16 +16,6 @@ class Bio extends Component {
             </div>
           </div>
           <div className="col-xs-12 col-md-6">
-            <h2>H2</h2>
-            <hr
-              style={{
-                width: "80%",
-                height: "4px",
-                border: "none",
-                color: "#333",
-                backgroundColor: "#333"
-              }}
-            />
             <p>
               Kyle J Smith is the founder of Determination, Incorporated. He is
               an entrepreneurial ecosystem builder in Kansas City, Missouri.
@@ -33,12 +23,29 @@ class Bio extends Component {
               central hub for all things entrepreneurship, Kyle started
               Determination, Incorporated.
             </p>
-            <h3>H3</h3>
+            <hr
+              style={{
+                width: "60%",
+                height: "4px",
+                border: "none",
+                color: "#333",
+                backgroundColor: "#333"
+              }}
+            />
             <p>
               Kyle volunteers as a community organizer with 1 Million Cups
               Kansas City, the birthplace of the Kauffman Foundation-supported
               program, and a Startup Huddle ambassador with the Global
               Entrepreneurship Network.
+              <hr
+                style={{
+                  width: "60%",
+                  height: "4px",
+                  border: "none",
+                  color: "#333",
+                  backgroundColor: "#333"
+                }}
+              />
               <p>
                 <a href="mailto:bdburns6389@gmail.com">Email Him</a>
               </p>
